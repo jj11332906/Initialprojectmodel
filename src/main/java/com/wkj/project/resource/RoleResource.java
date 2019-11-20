095dfe8fb708530f1a12ffe244efb0295369ad52
weikejun <11332906@qq.com>
2019-11-20 16:54:40 +0800

    wkj

--- src/main/java/com/wkj/project/resource/RoleResource.java
+++ src/main/java/com/wkj/project/resource/RoleResource.java
@@ -1,10 +1,7 @@
 package com.wkj.project.resource;
 
 
-import com.wkj.project.dto.SysAuthorityDTO;
 import com.wkj.project.dto.SysRoleDTO;
-import com.wkj.project.entity.SysAuthority;
-import com.wkj.project.service.AuthorityService;
 import com.wkj.project.service.SysRoleService;
 import com.wkj.project.util.ErrorCode;
 import com.wkj.project.util.Result;
@@ -12,8 +9,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,7 +31,7 @@ public class RoleResource {
     public Result list(
     ) {
         log.info("获取角色数据");
-        List<SysRoleDTO> sysRoleDTOS= sysRoleService.findAll();
+        List<SysRoleDTO> sysRoleDTOS = sysRoleService.findAll();
         return Result.getResult(ErrorCode.OP_SUCCESS, sysRoleDTOS);
     }
 
